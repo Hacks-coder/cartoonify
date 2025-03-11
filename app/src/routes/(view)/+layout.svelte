e61f08657b49a79fa5f6fc4c9017929e4afaa5fd
Hacks-coder <ajheartcry@gmail.com>
2025-03-11 00:10:10 -0700

    first commit

---
+++ app/src/routes/(view)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Header from '../../Header/Header.svelte';
+	import Hero from '../../Main/others/Hero.svelte';
+
+	let { children } = $props();
+</script>
+
+<div>
+    <div>
+        <Header />
+        <Hero />
+    </div>
+    {@render children()}
+</div>
---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '@fontsource/krona-one';
+	import '@fontsource/inria-sans';
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="bg-black text-sm text-white font-main">
+	{@render children()}
+</div>
